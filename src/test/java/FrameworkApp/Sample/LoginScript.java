3c2fd029d20de303eac9330be9c4ea87e10daad7
Gajendra <gajendra.akash022gmail.com>
2018-08-23 07:41:39 +0530

    update with testcscript

--- src/test/java/FrameworkApp/Sample/LoginScript.java
+++ src/test/java/FrameworkApp/Sample/LoginScript.java
@@ -1,10 +1,14 @@
 package FrameworkApp.Sample;
 
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.Test;
 
 import FrameworkApp.Sample.Common.Common;
+import FrameworkApp.Sample.Common.Initilization;
+import FrameworkApp.Sample.ExcelRead.ExcelRead;
 import FrameworkApp.Sample.PageObject.LoginObject;
 import FrameworkApp.Sample.testng.Dataprovider;
 
@@ -13,10 +17,46 @@ public class LoginScript {
 
 	@Test(dataProvider = "login", dataProviderClass = Dataprovider.class)
 
-	public void loginapp(String user, String pwd) {
+	public void loginapp(String InputType, String user, String pwd, String ManaualStep, String Expected) throws InterruptedException, IOException {
 
-		logger.info("Login Validation Started");
-		new LoginObject().LoginObject(user, pwd);
-		logger.info("Login Validation Ended");
+		if (InputType.contains("N")) {
+
+			logger.info("TestCase:" + ManaualStep);
+			new LoginObject().LoginObject(user, pwd);
+			new LoginObject().Loginbutton().click();
+			new Common().scriptpause(10);
+			new Common().waitElementTobevisible(new LoginObject().LoginErrorMessgae(), 10);
+
+			if (Expected.trim().toUpperCase().equals(new LoginObject().LoginErrorMessgae().getText().trim().toUpperCase())) {
+				logger.assertLog(true, Expected);
+				String[] dataToWrite = { "PASS" };
+
+				new ExcelRead().writeData(Initilization.path, Initilization.filename, Initilization.Sheet, dataToWrite);
+			} else {
+				logger.assertLog(false, Expected);
+				String[] dataToWrite = { "FAIL" };
+/*				new ExcelRead().writeData(Initilization.path, Initilization.filename, Initilization.Sheet, dataToWrite);
+*/			}
+
+		} else {
+			new LoginObject().LoginObject(user, pwd);
+			new LoginObject().Loginbutton().click();
+			new Common().scriptpause(10);
+			new LoginObject().waitElementTobevisible(new LoginObject().LoginSuccesMessage(), 5);
+
+			logger.info("TestCase" + ManaualStep);
+
+			if (Expected.trim().toUpperCase().equals(new LoginObject().LoginSuccesMessage().getText().trim().toUpperCase())) {
+				logger.assertLog(true, Expected);
+				String[] dataToWrite = { "PASS" };
+
+/*				new ExcelRead().writeData(Initilization.path, Initilization.filename, Initilization.Sheet, dataToWrite);
+*/
+			} else {
+				logger.assertLog(false, Expected);
+				String[] dataToWrite = { "FAIL" };
+/**/			}
+		}
+
 	}
 }
